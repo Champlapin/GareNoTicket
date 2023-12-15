e587c4be2fed88db0bb0bc66ac710be3e8102ee5
Champlapin <105014878+Champlapin@users.noreply.github.com>
2023-12-14 19:23:28 -0500

    Update user.js

--- Api/routes/user.js
+++ Api/routes/user.js
@@ -3,12 +3,14 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
+const isAuth = require("../middleware/is-auth");
+
 
 // /user/ => GET
 router.get("/users/", usersController.getUsers);
 
 // L'utilisateur actuellement connecté /user/ (profil utilisateur)
-router.get("/user/", usersController.getUser);
+router.get("/user/",isAuth, usersController.getUser);
 
 // un utilisateur avec son id /user/:id
 router.get("/user/:id", usersController.getUserById);
@@ -17,7 +19,7 @@ router.get("/user/:id", usersController.getUserById);
 router.put("/user/:userId", usersController.updateUser);
 
 // Suppression de l'utilisateur actuellement connecté
-router.delete("/user/", usersController.deleteUser);
+router.delete("/user/",isAuth, usersController.deleteUser);
 
 // Mise à jour d'une voiture
 router.put("/car/:userId", usersController.updateCar);
